832006a0f9eea086b2ee0a85a8cae409954a9c3f
kmh0228 <kmh0228@sina.com>
2017-03-27 14:38:19 +0800

    add html2canvas

---
+++ tools/drag.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+//拖拽函数，当两个参数时，第二个参数是移动时间
+	function drag(obj,fndown,fnmove,fnup){
+		if(!obj)return;
+		if(arguments.length==2){
+			var fnmove=fndown;
+			fndown=null;
+		}
+		var mousedown=function(ev){
+			var oEvent = ev||event;
+			var oldX = oEvent.clientX;
+			var oldY = oEvent.clientY;
+			fndown&&fndown();
+			var mousemove=function(ev){
+				var oEvent = ev||event;
+				var newX = oEvent.clientX;
+				var newY = oEvent.clientY;
+				fnmove&&fnmove(newX-oldX,newY-oldY);
+			};
+			document.addEventListener('mousemove',mousemove);
+			
+			var mouseup=function(){
+				document.removeEventListener('mousemove',mousemove);
+				document.removeEventListener('mousemove',mousemove);
+				fnup&&fnup();
+				obj.releaseCapture&&obj.releaseCapture();
+			};
+			document.addEventListener('mouseup',mouseup);
+			obj.setCapture&&obj.setCapture();
+			ev.preventDefault();
+		};
+		obj.addEventListener('mousedown',mousedown);
+	}
+
