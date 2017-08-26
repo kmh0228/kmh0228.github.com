5d106613edc65c8d45cdb22c941d14721be7217d
kmh0228 <kmh0228@sina.com>
2017-08-26 22:11:20 +0800

    update

--- study/nodejs/server.js
+++ study/nodejs/server.js
@@ -6,7 +6,9 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  
+  res.write('hel');
+  res.end();
 });
 
 server.listen(port, hostname, () => {
