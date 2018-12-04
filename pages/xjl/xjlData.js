596f9dc07adff5c6219d50a3b91b95c0ca487a94
kmh0228 <kmh0228@sina.com>
2018-12-04 16:16:36 +0800

    update xiangjiulingdatiyemian

--- pages/xjl/xjlData.js
+++ pages/xjl/xjlData.js
@@ -1,13 +1,4 @@
 
-var arr = [];
-for(var i=0;i<100;i++){
-    arr.push({"wx":''+Math.random()+Math.random()+Math.random(),"da":Math.random()});
-}
-
-
-
-window.xjlData = arr;
-
-// window.xjlData = [
-//     {"wx":"你chou啥","da":"抽你炸地"}
-// ]
+window.xjlData = [
+    {"question":"你chou啥","opt1":"抽你炸地"}
+]
