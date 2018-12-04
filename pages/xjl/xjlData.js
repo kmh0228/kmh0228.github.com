3799d1129ef2844a2abe472b3f32eb20234ba20b
kmh0228 <kmh0228@sina.com>
2018-12-04 11:25:07 +0800

    update

--- pages/xjl/xjlData.js
+++ pages/xjl/xjlData.js
@@ -1,4 +1,13 @@
 
-window.xjlData = [
-    {"wx":"你chou啥","da":"抽你炸地"}
-]
+var arr = [];
+for(var i=0;i<100;i++){
+    arr.push({"wx":''+Math.random()+Math.random()+Math.random(),"da":Math.random()});
+}
+
+
+
+window.xjlData = arr;
+
+// window.xjlData = [
+//     {"wx":"你chou啥","da":"抽你炸地"}
+// ]
