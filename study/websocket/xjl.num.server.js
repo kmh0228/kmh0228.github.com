7ff4b0b9b306116c94e8df4258feb0eea4a52b0d
kmh0228 <kmh0228@sina.com>
2019-03-25 16:16:20 +0800

    update

--- study/websocket/xjl.num.server.js
+++ study/websocket/xjl.num.server.js
@@ -1,12 +1,29 @@
-var webs = require('ws');
-var fs = require('fs');
+var https=require('https');
+var ws=require('ws');
+var fs=require('fs');
+var keypath='/path/to/private.pem';
+var certpath='/path/to/file.crt';
 
-var wss = new webs.Server({
-    port:8081,
-    perMessageDeflate:{
-
-    }
-});
+var options = {
+  key: fs.readFileSync(keypath),
+  cert: fs.readFileSync(certpath),
+  passphrase:'idpbg1230.'//如果秘钥文件有密码的话，用这个属性设置密码
+};
+ 
+var server=https.createServer(options, function (req, res) {//要是单纯的https连接的话就会返回这个东西
+    res.writeHead(403);//403即可
+    res.end("This is a  WebSockets server!\n");
+}).listen(8081);
+ 
+ 
+var wss = new ws.Server( { server: server } );//把创建好的https服务器丢进websocket的创建函数里，ws会用这个服务器来创建wss服务
+//同样，如果丢进去的是个http服务的话那么创建出来的还是无加密的ws服务
+// wss.on( 'connection', function ( wsConnect ) {
+//     wsConnect.on( 'message', function ( message ) {
+//         console.log( message );
+//     });
+    
+// });
 
 wss.on('connection', function connection(ws) {
     console.log('链接成功');
