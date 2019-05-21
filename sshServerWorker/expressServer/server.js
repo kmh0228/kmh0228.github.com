fcb4812453e096cbfdda0a69f4103cb76d934d9e
kmh0228 <kmh0228@sina.com>
2019-05-21 10:03:41 +0800

    update

--- sshServerWorker/expressServer/server.js
+++ sshServerWorker/expressServer/server.js
@@ -4,6 +4,6 @@ var path = require('path');
 //创建一个app对象，类似一个web 应用（网站）
 var app = express();
 //托管静态文件
-app.use(express.static(path.join(__dirname,'web/dist')))
+app.use(express.static(path.join(__dirname, '../web/dist')))
 //接受指定路径的请求，指定回调函数
-app.listen(8000, () => console.log('Example app listening on port 80!'))
+app.listen(80, () => console.log('Example app listening on port 80!'))
