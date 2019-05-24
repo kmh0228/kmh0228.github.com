90845c1c0824a539c59cc6426bd5c3162a1e0e94
kmh0228 <kmh0228@sina.com>
2019-05-24 14:51:30 +0800

    update

--- sshServerWorker/8080/server.js
+++ sshServerWorker/8080/server.js
@@ -1,8 +1,13 @@
-var https=require('https');
+//1、引入模块  使用的http服务协议是RFC2616  nodejs的作者已经写好了，直接引入就行
+var http = require('http');
+//2、创建服务器    函数的参数req是发送给服务器的请求，res是服务器的相应
 var n = 0;
-var server=https.createServer(function (req, res) {//要是单纯的https连接的话就会返回这个东西
-    n++;
-    console.log('收到一个请求,' + n);
-    res.end("请求成功.152.136.79.68:8080");
+var httpObj = http.createServer(function(req,res){
+            n++;
+            console.log('收到请求:' + n);
+            res.write('请求成功');
+            res.end();    
+        
 });
-server.listen(8080);
+//监听端口
+httpObj.listen(8080);
