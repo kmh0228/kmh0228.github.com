89ad1aa39f9ab23f7bf21eba898f2ea33cb345c3
kmh0228 <kmh0228@sina.com>
2019-05-24 14:43:45 +0800

    udpate

--- sshServerWorker/8080/server.js
+++ sshServerWorker/8080/server.js
@@ -1,6 +1,6 @@
 var https=require('https');
 var n = 0;
-var server=https.createServer(options, function (req, res) {//要是单纯的https连接的话就会返回这个东西
+var server=https.createServer(function (req, res) {//要是单纯的https连接的话就会返回这个东西
     n++;
     console.log('收到一个请求,' + n);
     res.end("请求成功.152.136.79.68:8080");
