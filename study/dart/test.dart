e777cf532090a9589dbebbbb06e623e36ae4c544
kmh0228 <kmh0228@sina.com>
2019-06-14 14:42:18 +0800

    udpate

---
+++ study/dart/test.dart
@@ -0,0 +1,12 @@
+
+//定义一个函数
+printNumber(num aNumber) {
+  print('The number is $aNumber.'); // 输出至控制台.
+}
+
+// 此处是APP开始执行的地方
+main() {
+  print('what');
+  var number = 42; // 声明并初始化变量
+  printNumber(number); // 调用函数
+}
