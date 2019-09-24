381acda29bfef0053d6eb0898a75407e2a87532e
kmh0228 <kmh0228@sina.com>
2019-09-24 09:39:50 +0800

    update

--- sshServerWorker/web/src/views/piano/piano.vue
+++ sshServerWorker/web/src/views/piano/piano.vue
@@ -12,6 +12,7 @@
 
             </div>
         </div>
+        <div class="load" v-show="load"><br><br><br>{{loadHTML}}</div>
     </div>
 </template>
 <script>
@@ -28,7 +29,9 @@ export default {
             ],
             typeChats:['A','B','C','D','E','F','G'],     //七个音标
             typeNums:[0,1,2,3,4,5,6,7,8],
-            audioSrc:''
+            audioSrc:'',
+            load:true,
+            loadHTML:'加载中。。。剩余0个音频'
         }
     },
     methods:{
@@ -43,11 +46,20 @@ export default {
     },
     mounted(){
         this.pianoWhiteKeys = [];
+        var loadNum=0;
+        var _this = this;
+        function loaded(){
+            loadNum--;
+            _this.loadHTML = '加载中。。。剩余'+loadNum+'个音频';
+            if(!loadNum)_this.load = false;
+        }
         for(var i = 0;i<52;i++){
+            loadNum++;
             var chart = this.typeChats[i%7];
             var num = this.typeNums[parseInt((i+5)/7)];
             var audio = document.createElement('audio');
             audio.src = '/keySconds/'+chart+'/'+chart+num+'.MP3';
+            audio.oncanplay = loaded;
             this.pianoWhiteKeys.push({audio:audio});
 
         }
@@ -55,10 +67,12 @@ export default {
         var indexs = [0,2,3,5,6,7,9,10,12,13,14,16,17,19,20,21,23,24,26,27,28,30,31,33,34,35,37,38,40,41,42,44,45,47,48,49];
         var indexsLength = indexs.length;
         for(var i = 0;i<indexsLength;i++){
+            loadNum++;
             var chart = this.typeChats[indexs[i]%7];
             var num = this.typeNums[parseInt((indexs[i]+5)/7)];
             var audio = document.createElement('audio');
             audio.src = escape('/keySconds/#'+chart+'/#'+chart+num+'.MP3');
+            audio.oncanplay = loaded;
             this.pianoBlankKeys.push({i:indexs[i],audio:audio});
         }
     }
@@ -66,7 +80,7 @@ export default {
 </script>
 <style lang="less">
 .piano_container{
-    width:100%; height:100%; overflow: hidden;
+    width:100%; height:100%; overflow: hidden; position:relative;
     .piano:after{
         content:''; display:block; clear:both;
     }
@@ -78,6 +92,9 @@ export default {
         .painoBlankKey{
             position:absolute; top:0; height:100px; background: #000;
         }
+    }
+    .load{
+        background: rgba(255,255,255,.6); position:absolute; top:0; left:0; height:100%; width:100%; text-align: center;
     }
 }
 </style>
