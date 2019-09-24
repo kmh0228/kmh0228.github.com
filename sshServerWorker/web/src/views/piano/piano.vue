94181b0f4a35f3e357b7a2f42933dfe831f2106c
kmh0228 <kmh0228@sina.com>
2019-09-24 09:01:56 +0800

    update

--- sshServerWorker/web/src/views/piano/piano.vue
+++ sshServerWorker/web/src/views/piano/piano.vue
@@ -5,7 +5,10 @@
             <div class="paionWhiteKey" v-for="(item,i) in pianoWhiteKeys" :style="{width:100/pianoWhiteKeys.length+'%'}" @click="whiteClick(i)">
                 
             </div>
-            <div class="painoBlankKey" v-for="(item,i) in pianoBlankKeys" :style="{width:200/3/pianoWhiteKeys.length+'%',left:item.i*100/pianoWhiteKeys.length-(100/3/pianoWhiteKeys.length)+'%'}">
+            <div class="painoBlankKey" v-for="(item,i) in pianoBlankKeys"
+                :style="{width:200/3/pianoWhiteKeys.length+'%',left:item.i*100/pianoWhiteKeys.length+(200/3/pianoWhiteKeys.length)+'%'}"
+                @click="blankClick(item.i)"
+            >
 
             </div>
         </div>
@@ -32,7 +35,21 @@ export default {
         whiteClick(index){
             var chart = this.typeChats[index%7];
             var num = this.typeNums[parseInt((index+5)/7)];
-            this.audioSrc = '/keySconds/'+chart+'/'+chart+num+'.MP3';
+            this.play('/keySconds/'+chart+'/'+chart+num+'.MP3');
+            //this.audioSrc = '/keySconds/'+chart+'/'+chart+num+'.MP3';
+        },
+        blankClick(index){
+            var chart = this.typeChats[index%7];
+            var num = this.typeNums[parseInt((index+5)/7)];
+            this.play(escape('/keySconds/#'+chart+'/#'+chart+num+'.MP3'));
+            //this.audioSrc = escape('/keySconds/#'+chart+'/#'+chart+num+'.MP3');
+        },
+        play(src){
+            var audio = document.createElement('audio');
+            //audio.autoplay = 'true';
+            audio.src = src;
+            //document.body.appendChild(audio);
+            audio.play();
         }
     },
     mounted(){
@@ -41,7 +58,7 @@ export default {
             this.pianoWhiteKeys.push({});
         }
         this.pianoBlankKeys = [];
-        var indexs = [1,3,4,6,7,8,10,11,13,14,15,17,18,20,21,22,24,25,27,28,29,31,32,34,35,36,38,39,41,42,43,45,46,48,49,50];
+        var indexs = [0,2,3,5,6,7,9,10,12,13,14,16,17,19,20,21,23,24,26,27,28,30,31,33,34,35,37,38,40,41,42,44,45,47,48,49];
         var indexsLength = indexs.length;
         for(var i = 0;i<indexsLength;i++){
             this.pianoBlankKeys.push({i:indexs[i]});
@@ -56,7 +73,7 @@ export default {
         content:''; display:block; clear:both;
     }
     .piano{
-        width:96%; margin: 20px auto; position:relative;
+        width:96%; margin: 20px auto; position:relative; cursor: pointer;
         .paionWhiteKey{
             float:left; border:1px solid #ccc; box-sizing: border-box; height:200px;
         }
