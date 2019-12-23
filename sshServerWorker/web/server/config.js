1655e3f27f11ad3e0c47e5d9b1cbf15df9101627
kmh0228 <kmh0228@sina.com>
2019-12-23 10:00:08 +0800

    update

--- sshServerWorker/web/server/config.js
+++ sshServerWorker/web/server/config.js
@@ -5,7 +5,7 @@ var devType = require('./devType');          // local 本地  server 服务
 
 module.exports = {
     // 存放数据的文件目录
-    dataUrl:devType=='local'?path.join(__dirname,'./data'):path.join(__dirname,'../../../../home/s5ming/data'),
+    dataUrl:devType=='local'?path.join(__dirname,'./data'):'/home/s5ming/data',
     //服务监控的端口
     serverPost:devType=='local'?9999:80,
 
