a0ccd5bff679f98811d4cfe9383a03f9c4056907
kmh0228 <kmh0228@sina.com>
2020-01-13 11:04:21 +0800

    update

--- python/pythonwx/sendwx.py
+++ python/pythonwx/sendwx.py
@@ -2,32 +2,61 @@
 import itchat
 import datetime
 import time
+import random
 
 toUserNames=[
-    "栖木26 凯月",
-    # "赵明强"
+    "凯月",
+    "骚军",
+    "高习军",
+    "狗狗",
+    "大妹",
+    "小妹",
+    "邵莉",
+    "深圳徒弟",
+    "糖果",
+    "李志杰",
+    "小蒋"
 ]
 
 times = [
-    {'h':8,'m':0,'msg':'good morning,美好的一天开始开始了.'},
-    {'h':22,'m':0,'msg':'good night,做个好梦.'}
+    {'h':8,'m':0,'msg':[
+        'good morning,美好的一天开始开始了.',
+        '早.',
+        '早安.',
+        '早啊.',
+        '早,起床啦。',
+        'good morning.'
+    ]},
+    {'h':22,'m':0,'msg':[
+        'good night,做个好梦.',
+        '晚安.',
+        '晚安,早些睡觉.',
+        'Sleep tight',
+        'sweet dreams',
+        'Nighty-night'
+    ]}
 ]
 
-def send_move(message):    
+def send_move(saytimemsg):    
     for touser in toUserNames:
+        saytimemsglength = len(saytimemsg)
+        rand = random.randint(0,saytimemsglength-1)
         users = itchat.search_friends(name=touser)
         if(len(users)>0):
-            itchat.send(message,toUserName=users[0]['UserName'])
+            itchat.send(saytimemsg[rand],toUserName=users[0]['UserName'])
 
 
 if __name__ == "__main__":
     itchat.auto_login(hotReload=True,enableCmdQR=2)  # 首次扫描登录后后续自动登录
-    send_move("test")
 
 while True:
     now = datetime.datetime.now()
+    if(now.hour == 0 and now.minute == 0):
+        for saytime in times:
+            saytime['m'] = random.randint(0,10)
     for saytime in times:
         if(now.hour == saytime['h'] and now.minute == saytime['m']):
-            send_move(saytime['msg'])
+            saytimemsg = saytime['msg']
+            send_move(saytimemsg)
     # 每隔60秒检测一次
     time.sleep(60)
